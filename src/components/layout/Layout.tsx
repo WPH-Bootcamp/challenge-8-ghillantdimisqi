91985674af379228728a765d78e3569037fc4851
ghillantdimisqi <ghillantdimisqi@gmail.com>
2026-05-31 11:02:32 +0700

    tambahan

--- src/components/layout/Layout.tsx
+++
@@ -1,9 +0,0 @@
-interface NavbarProps {
-  // Define your props here
-}
-
-const Navbar = ({} /* props */ : NavbarProps) => {
-  return <nav>{/* Your navbar implementation */}</nav>;
-};
-
-export default Navbar;
